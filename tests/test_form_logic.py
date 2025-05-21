786e75c745971b62131153aa6214c89d75d40c33
jeihyde25 <cormyr10@gmail.com>
2025-05-21 19:06:15 +0800

    added test for form logic

--- tests/test_form_logic.py
+++ tests/test_form_logic.py
@@ -1,4 +1,5 @@
 from src.form_logic import process_player_form
+from src.player import Player
 
 
 def test_process_player_success():
@@ -12,4 +13,16 @@ def test_process_player_empty_name():
     players = []
     msg = process_player_form("", "Setter", "Beginner", players)
     assert msg == "❌ Name cannot be empty"
+    assert len(players) == 0
+
+def test_process_player_empty_position():
+    players = []
+    msg = process_player_form("Jei", "Select...", "Beginner", players)
+    assert msg == "❌ Position and/or Skill Level cannot be empty"
+    assert len(players) == 0
+
+def test_process_player_empty_skill_level():
+    players = []
+    msg = process_player_form("Je", "Setter", "Select...", players)
+    assert msg == "❌ Position and/or Skill Level cannot be empty"
     assert len(players) == 0
