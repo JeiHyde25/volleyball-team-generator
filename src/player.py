dbacf0848fedfce3f4ac6a09a06576b1f050499d
jeihyde25 <cormyr10@gmail.com>
2025-05-21 15:49:27 +0800

    fix formatting for black

--- src/player.py
+++ src/player.py
@@ -1,16 +1,19 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Player:
     name: str
     position: str
     skill_level: str
 
-def create_player(name: str="", position: str="", skill_level: str=""):
+
+def create_player(name: str = "", position: str = "", skill_level: str = ""):
     validate_player_fields(name, position, skill_level)
-    return Player(name=name.strip(),
-                  position=position.strip(),
-                  skill_level=skill_level.strip())
+    return Player(
+        name=name.strip(), position=position.strip(), skill_level=skill_level.strip()
+    )
+
 
 def validate_player_fields(name: str, position: str, skill_level: str):
     if not name:
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,18 +1,14 @@
 from src.player import Player, create_player
 import pytest
 
-VALID_POSITIONS = [
-    "Setter",
-    "Outside Hitter",
-    "Middle Blocker",
-    "Opposite"
-]
+VALID_POSITIONS = ["Setter", "Outside Hitter", "Middle Blocker", "Opposite"]
 
 VALID_SKILL_LEVELS = [
     "Beginner",
     "Intermediate",
     "Advanced",
 ]
+
 
 def test_create_player_valid():
     player = create_player("Harold", "Setter", "Intermediate")
@@ -21,25 +17,31 @@ def test_create_player_valid():
     assert player.position == "Setter"
     assert player.skill_level == "Intermediate"
 
+
 def test_create_player_invalid():
     with pytest.raises(ValueError, match="Name cannot be empty"):
         create_player("", "Setter", "Intermediate")
+
 
 def test_create_player_invalid_position():
     with pytest.raises(ValueError, match="Position cannot be empty"):
         create_player("Setter", "", "Intermediate")
 
+
 def test_create_player_invalid_skill_level():
     with pytest.raises(ValueError, match="Skill level cannot be empty"):
         create_player("Harold", "Setter", "")
+
 
 def test_create_player_begin_name_in_whitespace():
     player = create_player("  Harold", "Setter", "Intermediate")
     assert player.name == "Harold"
 
+
 def test_create_player_end_name_in_whitespace():
     player = create_player("Harold  ", "Setter", "Intermediate")
     assert player.name == "Harold"
+
 
 def test_create_player_begin_end_name_in_whitespace():
     player = create_player("  Harold  ", "Setter", "Intermediate")
